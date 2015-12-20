b21833efb585fbe8f69768dc51d7ec6e20906d69
MurrDogg4 <rm643dplay@gmail.com>
2015-12-20 12:19:24 -0600

    fixed date delete

--- rotogurunba.py
+++ rotogurunba.py
@@ -162,7 +162,7 @@ def addtoDb(con, playerDict):
                         homeaway, pos, start, dksal, fdsal, dkp, fdp) \
                         VALUES ("'"%s"'", "'"%s"'", "'"%s"'", "'"%s"'", "'"%s"'", "'"%s"'", "'"%s"'", \
                                 "'"%s"'", "'"%s"'", "'"%s"'", "'"%s"'", "'"%s"'", "'"%s"'", "'"%s"'")" % \
-                    (day, key, playerDict[day][key]['fullNm'], playerDict[day][key]['lastNm'], playerDict[day][key]['firstNm'], playerDict[day][key]['team'], playerDict[day][key]['opp'], \
+                    (date, key, playerDict[day][key]['fullNm'], playerDict[day][key]['lastNm'], playerDict[day][key]['firstNm'], playerDict[day][key]['team'], playerDict[day][key]['opp'], \
                     playerDict[day][key]['homeaway'], playerDict[day][key]['pos'], playerDict[day][key]['start'], playerDict[day][key]['dksal'], playerDict[day][key]['fdsal'], playerDict[day][key]['dkp'], playerDict[day][key]['fdp'])
                 x = con.cursor()
                 x.execute(query)
