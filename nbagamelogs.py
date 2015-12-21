f44d5807e63a08a1858514063f9f780355121ef5
MurrDogg4 <rm643dplay@gmail.com>
2015-12-20 18:13:05 -0600

    database work

--- nbagamelogs.py
+++ nbagamelogs.py
@@ -22,6 +22,76 @@ def playergamedata():
         gamelist.append(playerdict)
         playerdict = {}
 
-    return gamelist[0]
+    return gamelist
+
+def addtoDb(gamelist, datestr, con):
     
-print playergamedata()
+    headers = ["SEASON_ID", "PLAYER_NAME", "TEAM_ABBREVIATION", "TEAM_NAME", "GAME_ID", "MATCHUP", \
+    "WL", "MIN", "FGM", "FGA", "FG_PCT", "FG3M", "FG3A", "FG3_PCT", "FTM", "FTA", "FT_PCT", "OREB", "DREB", \
+    "REB", "AST", "STL", "BLK", "TOV", "PF", "PTS", "PLUS_MINUS"]
+    
+    ### other available headers: 'VIDEO_AVAILABLE', 'PLAYER_ID', 'GAME_DATE'
+    
+    playerdict = {}
+    playerdict[datestr] = {}
+    for i in gamelist:
+        if i['GAME_DATE'] == datestr:
+            playerid = i['PLAYER_ID']
+            playerdict[datestr][playerid] = {}
+            for header in headers:
+                playerdict[datestr][playerid][header] = i[header]
+            
+    query = "DELETE FROM nba_gamelog WHERE day = %s" % (datestr)
+    x = con.cursor()
+    x.execute(query)
+    
+    try:
+        for key in playerdict[datestr].keys():
+            with con:
+                query = "INSERT INTO nba_gamelog (day, season_id, player_id, playernm_full, team_abbr, team, game_id, matchup, \
+                        wl, min, fgm, fga, fg_pct, fg3m, fg3a, \
+                        fg3_pct, ftm, fta, ft_pct, oreb, dreb, reb, \
+                        ast, stl, blk, tov, pf, pts, plus_minus) \
+                        VALUES ("'"%s"'", "'"%s"'", "'"%s"'", "'"%s"'", "'"%s"'", "'"%s"'", "'"%s"'", "'"%s"'", \
+                                "'"%s"'", "'"%s"'", "'"%s"'", "'"%s"'", "'"%s"'", "'"%s"'", "'"%s"'", \
+                                "'"%s"'", "'"%s"'", "'"%s"'", "'"%s"'", "'"%s"'", "'"%s"'", "'"%s"'", \
+                                "'"%s"'", "'"%s"'", "'"%s"'", "'"%s"'", "'"%s"'", "'"%s"'", "'"%s"'")" % \
+                    (datestr, playerdict[datestr][key]['SEASON_ID'], key, playerdict[datestr][key]['PLAYER_NAME'], playerdict[datestr][key]['TEAM_ABBREVIATION'], playerdict[datestr][key]['TEAM_NAME'], playerdict[datestr][key]['GAME_ID'], playerdict[datestr][key]['MATCHUP'], \
+                    playerdict[datestr][key]['WL'], playerdict[datestr][key]['MIN'], playerdict[datestr][key]['FGM'], playerdict[datestr][key]['FGA'], playerdict[datestr][key]['FG_PCT'], playerdict[datestr][key]['FG3M'], playerdict[datestr][key]['FG3A'], \
+                    playerdict[datestr][key]['FG3_PCT'], playerdict[datestr][key]['FTM'], playerdict[datestr][key]['FTA'], playerdict[datestr][key]['FT_PCT'], playerdict[datestr][key]['OREB'], playerdict[datestr][key]['DREB'], playerdict[datestr][key]['REB'], \
+                    playerdict[datestr][key]['AST'], playerdict[datestr][key]['STL'], playerdict[datestr][key]['BLK'], playerdict[datestr][key]['TOV'], playerdict[datestr][key]['PF'], playerdict[datestr][key]['PTS'], playerdict[datestr][key]['PLUS_MINUS'])
+                x = con.cursor()
+                x.execute(query)
+    except:
+        continue
+            
+    return playerdict
+    
+def main():
+    
+    datelist = []
+    
+    con = MySQLdb.connect(host='mysql.server', user='MurrDogg4', passwd='syracuse', db='MurrDogg4$dfs-nba')
+    
+    year = 2015
+    for i in range(10,13):
+        for x in range(1,32):
+            if i < 10:
+                month = '0' + str(i)
+            else:
+                month = str(i)
+            if x < 10:
+                day = '0' + str(x)
+            else:
+                day = str(x)
+                
+            datestr = str(year) + "-" + month + "-" + day
+            datelist.append(datestr)
+            
+    print datelist
+    
+    for dates in datelist:
+        print addtoDb(playergamedata(), dates, con)
+    
+if __name__ == '__main__':
+    main()
